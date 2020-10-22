27bc5c2175aba730d43836183a4a689c2c045996
demuko <tenkose@hotmail.com>
2020-10-22 14:59:22 +0900

    reduce sleep time

--- workspace/touch/app.c
+++ workspace/touch/app.c
@@ -8,7 +8,7 @@ void main_task(intptr_t exinf) {
 
 	ev3_lcd_draw_string("Waiting...", 0, 30);
 	while(!ev3_touch_sensor_is_pressed(TOUCH_PORT)){        /* タッチセンサの状態を確認 */
-		tslp_tsk(100000);                                   /* 100,000us刻みで実行 */
+		tslp_tsk(1000);                                   /* 100,000us刻みで実行 */
 	}
 	ev3_lcd_draw_string("Pressed!", 0, 30);                 /* タッチセンサが押されたことを表示 */
 
