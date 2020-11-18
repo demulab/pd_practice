2bba2339fa6bcaaa5d5bc3ef3032abe155308da4
demuko <tenkose@hotmail.com>
2020-11-18 10:42:45 +0900

    add a PID control program

---
+++ workspace/pid_cyc/app.h
@@ -0,0 +1,8 @@
+extern void	main_task(intptr_t exinf);
+extern void	ev3_cyc1(intptr_t exinf);
+extern void	ev3_cyc2(intptr_t exinf);
+extern void	ev3_cyc3(intptr_t exinf);
+extern void	ev3_cyc4(intptr_t exinf);
+
+void RingTone(int freq, int time, int vol);
+
